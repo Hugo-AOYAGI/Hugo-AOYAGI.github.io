2076d4c137723ad22e2f491842d82bbb71bdbd2e
Hugo-AOYAGI <aoyagihugo@gmail.com>
2020-07-06 11:07:41 +0200

    added project card

--- index.js
+++ index.js
@@ -1,53 +1,87 @@
 
-let error_bait = 0;
 
-let urls = ["./taupins", "./uimaker", "./404.html", "./404.html", "./404.html", "./404.html"]
-
-$(document).ready(function () {
-    setInterval(addChangingTitle, 3000);
-    addLangsLinks();
-    var j = 0;
-    for (let card of $(".project-card")) {
-        $(card).on("click", function () {
-          window.location = urls[$(".project-card").index($(this))];
-        });
-        j++;
+var projects = {
+    "Les Taupins": {
+        "short-desc": "A mobile app to learn everything there is to know in PTSI/PT.",
+        "thumbnail-name": "taupins",
+        "langs": ["flutter", "dart", "js", "electron"],
+        "type": "mobile",
+        "github-link": "https://github.com/Hugo-AOYAGI/prepa_quiz_app",
+        "license": "MIT",
+        "website": "www.hugo-aoyagi.me/taupins",
+        "links": ["https://play.google.com/store/apps/details?id=com.taupins.app"]
+    },
+    
+    "UI Maker": {
+        "short-desc": "Python desktop app to create tkinter user interfaces in a heartbeat.",
+        "thumbnail-name": "ui_maker",
+        "langs": ["python", "tkinter"],
+        "type": "desktop",
+        "github-link": "https://github.com/Hugo-AOYAGI/Python-Tkinter-UI-Maker",
+        "license": "MIT",
+        "website": "",
+        "links": []
+    },
+    
+    "ReMind": {
+        "short-desc": "A Todo App i built to learn electron.",
+        "thumbnail-name": "remind",
+        "langs": ["electron", "js", "html", "sass"],
+        "type": "desktop",
+        "github-link": "https://github.com/Hugo-AOYAGI/ToDoApp",
+        "license": "MIT",
+        "website": "",
+        "links": []
+    },
+    
+    "Life On Mars": {
+        "short-desc": "A game created for a school project.",
+        "thumbnail-name": "life_on_mars",
+        "langs": ["python", "tkinter", "pygame"],
+        "type": "desktop",
+        "github-link": "https://github.com/Hugo-AOYAGI/Life-on-mars-game",
+        "license": "MIT",
+        "website": "",
+        "links": []
+    },
+    
+    "Personal Website": {
+        "short-desc": "The website you are browsing.",
+        "thumbnail-name": "personal_website",
+        "langs": ["html", "js"],
+        "type": "web",
+        "github-link": "https://github.com/Hugo-AOYAGI/Hugo-AOYAGI.github.io",
+        "license": "MIT",
+        "website": "www.hugo-aoyagi.me",
+        "links": []
     }
-    
-    
-});
+
+};
 
 
-var titles = ["Programming beginner", "Student in Classe Preparatoire"];
-var i = 0;
-function addChangingTitle () {
-    i++;
-    $(".title").css("opacity", 0);
-    setTimeout(function () {
-        $(".title").html(titles[i%titles.length]);
-        $(".title").css("opacity", 1);
-    }, 800);
-} 
 
-function addLangsLinks() {
-   $(".tk").on("click", function() {
-       window.location = "https://docs.python.org/3/library/tkinter.html";
-   }); 
+
+
+document.addEventListener('DOMContentLoaded', function(){ 
     
-   $(".py").on("click", function() {
-       window.location = "https://www.python.org/";
-   });
+    var projects_container = document.getElementById("projects_container");
     
-    $(".flutter").on("click", function() {
-       window.location = "https://flutter.dev/";
-   });
-    
-    $(".dart").on("click", function() {
-       window.location = "https://dart.dev/";
-   });
-    
-    $(".dart").on("click", function() {
-       window.location = "https://www.electronjs.org/";
-   });
-}
+    Object.keys(projects).forEach((title) => {
+        
+    let node = document.createElement("div");
+    node.innerHTML = `<div class="project-card">
+              <img src="assets/thumbnails/${projects[title]["thumbnail-name"]}.png" alt="" class="project-background">
+              <div class="project-name-wrapper">
+                   <div class="project-name">
+                       ${title.toUpperCase()}
+                   </div>
+                   <div class="project-short-desc">
+                       ${projects[title]["short-desc"]}
+                   </div>
+              </div>
+        </div>`;
+        
+    projects_container.appendChild(node);
+    });
+}, false);
 
